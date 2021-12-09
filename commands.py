151db7fae76808d94338325c880f468dc79a7064
harry <cashel.harry@gmail.com>
2021-12-09 21:06:30 +1100

    new commands to generate entries in database tables

--- commands.py
+++ commands.py
@@ -20,15 +20,30 @@ def drop_db():
 @db_commands.cli.command("seed")
 def seed_db():
     from models.User import User
+    from models.Subreddit import Subreddit
     from faker import Faker
-    faker = Faker()
+    from main import bcrypt
+    import random
 
-    for i in range(20):
+    faker = Faker()
+    users = []
+
+    for i in range(5):
         user = User()
         user.email = f"user{i}@testing.com"
-        user.username = f"name{i}"
-        user.password = "123445678"
+        user.username = faker.name()
+        user.password = bcrypt.generate_password_hash("12345678").decode("utf-8")
         db.session.add(user)
+        users.append(user)
+
+    db.session.commit()
+
+    for i in range(10):
+        subreddit = Subreddit()
+        subreddit.name = faker.catch_phrase()
+        subreddit.description = "description"
+        subreddit.owner_id = random.choice(users).id
+        db.session.add(subreddit)
 
     db.session.commit()
     print("Tables seeded")
