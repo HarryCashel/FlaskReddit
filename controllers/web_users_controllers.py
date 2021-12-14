a6287c554a9dd3ce16876226788419b99ce2c13e
harry <cashel.harry@gmail.com>
2021-12-14 12:06:56 +1100

    login manager functions

--- controllers/web_users_controllers.py
+++ controllers/web_users_controllers.py
@@ -1,13 +1,22 @@
 from models.User import User
-from main import db
+from main import db, login_manager
 from schemas.UserSchema import user_schema
 from flask import Blueprint, render_template, flash, redirect, url_for, abort, request
 from flask_login import current_user, login_user, logout_user, login_required
 
 web_users = Blueprint("web_users", __name__, url_prefix="/web")
 
+
 @login_manager.user_loader
 def load_user(user_id):
     if user_id is not None:
         return User.query.get(user_id)
     return None
+
+
+@login_manager.unauthorized_handler
+def unauthorised():
+    flash("You must be logged in to view this page")
+    return redirect(url_for('web_users.web_users_login'))
+
+
