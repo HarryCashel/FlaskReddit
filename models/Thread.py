09f8e05da5e2bb229dae3c7e5ffa209e829ca824
harry <cashel.harry@gmail.com>
2022-02-18 10:34:26 +1100

    function to get timestamp of comments

--- models/Thread.py
+++ models/Thread.py
@@ -1,5 +1,5 @@
 from main import db
-
+from utils import pretty_date
 
 class Thread(db.Model):
     __tablename__ = "threads"
@@ -14,6 +14,22 @@ class Thread(db.Model):
 
     comment = db.relationship("Comment", backref="thread", lazy="dynamic")
 
+    # def __init__(self, title, content, thread_owner, parent_subreddit):
+    #     self.title = title
+    #     self.content = content
+    #     self.thread_owner = thread_owner
+    #     self.parent_subreddit = parent_subreddit
+
+    def __repr__(self):
+        return '<Thread %r>' % (self.title)
+
+    def get_comments(self, order_by="timestamp"):
+        """Function to return up to 50 comments that are children of a thread"""
+        if order_by == "timestamp":
+            return self.comment.filter_by(parent_thread=self.id).order_by(db.desc(Comment.date_created)).all()[:50]
+        else:
+            return self.comment.filter_by(parent_thread=self.id).order_by(db.desc(Comment.date_created)).all()[:50]
+
 
 class Comment(db.Model):
     __tablename__ = "comments"
@@ -27,3 +43,21 @@ class Comment(db.Model):
 
     parent_comment = db.Column(db.Integer, db.ForeignKey("comments.id"))
     children = db.relationship("Comment", backref=db.backref('parent', remote_side=[id]), lazy="dynamic")
+
+    # def __init__(self, content, parent_thread, comment_owner, parent_comment):
+    #     self.content = content
+    #     self.parent_thread = parent_thread
+    #     self.comment_owner = comment_owner
+    #     self.parent_comment = parent_comment
+
+    def get_comments(self, order_by="timestamp"):
+        """Function to return up to 50 comments that are children of a comment"""
+        if order_by == "timestamp":
+            return self.children.order_by(db.desc(Comment.date_created)).all()[:50]
+        else:
+            return self.children.order_by(db.desc(Comment.date_created)).all()[:50]
+
+    def show_time(self):
+        """Returns a human digestible version of the age of the comment"""
+        local = pretty_date(self.date_created)
+        return local
