ba3fceef3e4c03f96432fb8ee2df925c14c9b01d
harry <cashel.harry@gmail.com>
2022-02-18 10:35:49 +1100

    logic for thread routes

--- controllers/web_threads_controller.py
+++ controllers/web_threads_controller.py
@@ -16,12 +16,18 @@ from controllers.web_subreddits_controller import is_member, unauthorised
 web_threads = Blueprint("web_threads", __name__, url_prefix="/r")
 
 
-def get_comments(thread_id):
-    """Queries database for children comments.
-    Returns top comment."""
-    # thread = Thread.query.filter_by(id=thread_id).first()
-    comment = Comment.query.filter_by(parent_thread=thread_id)
-    return comment
+# def get_comments(thread_id):
+#     """Queries database for children comments.
+#     Returns top comment."""
+#     # thread = Thread.query.filter_by(id=thread_id).first()
+#     comment = Comment.query.filter_by(parent_thread=thread_id)
+#     return comment
+
+def get_comment_owner(comment_id):
+    """Function to query database for username of a comment owner"""
+    user = Comment.query.filter_by(id=comment_id).first().comment_owner
+    username = User.query.filter_by(id=user).first().username
+    return username
 
 
 @web_threads.route('/<int:thread_id>', defaults={'sub_name': None})
@@ -34,13 +40,14 @@ def show_thread(thread_id, sub_name):
     comment_form = CreateComment()
     user_id = current_user.get_id()
     subreddits = get_subreddits()
-    comments = get_comments()
     join = JoinSub()
     leave = LeaveSub()
+    users = User.query.all()
+
+    thread = Thread.query.filter_by(id=thread_id).first()
 
     # query for the subreddit name to pass through to the template
     if sub_name is None:
-        thread = Thread.query.filter_by(id=thread_id).first()
         parent_id = thread.parent_subreddit
         sub_name = Subreddit.query.filter_by(id=parent_id).first().name
 
@@ -49,13 +56,9 @@ def show_thread(thread_id, sub_name):
     if not current_subreddit:
         abort(404)
 
-    member = is_member(subreddit_name=sub_name, user_id=user_id)
-
-    thread = Thread.query.filter_by(id=thread_id).first()
-
     return render_template("view_thread.html", thread=thread, login_form=login_form, register_form=register_form,
                            subreddits=subreddits, join=join, leave=leave, current_subreddit=current_subreddit,
-                           comment_form=comment_form, comments=comments)
+                           comment_form=comment_form, owner=get_comment_owner)
 
 
 @web_threads.route("<int:thread_id>/create", methods=["POST"])
@@ -74,3 +77,26 @@ def create_comment(thread_id):
         db.session.commit()
 
         return redirect(url_for("web_threads.show_thread", thread_id=thread_id))
+
+
+@web_threads.route("<int:thread_id>/<int:comment>/create", methods=["POST"])
+@login_required
+def create_child_comment(thread_id, comment_id=None):
+    user_id = current_user.get_id()
+    comment_form = CreateComment()
+
+    if comment_id == None:
+        pass
+
+    if comment_form.submit.data:
+        comment = Comment()
+        comment.comment_owner = user_id
+        comment.parent_thread = thread_id
+        comment.parent_comment = comment_id
+        comment.content = comment_form.content.data
+
+        db.session.add(comment)
+        db.session.commit()
+
+        return redirect(url_for("web_threads.show_thread", thread_id=thread_id))
+
