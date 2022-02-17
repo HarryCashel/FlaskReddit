59813e27c6092e46bb8324bd7e7356b114134ca3
harry <cashel.harry@gmail.com>
2022-02-17 11:18:23 +1100

    added web_threads_controller.py

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,11 +3,13 @@ from controllers.subreddits_controller import subreddits
 from controllers.threads_controller import threads
 from controllers.web_users_controllers import web_users
 from controllers.web_subreddits_controller import web_reddits
+from controllers.web_threads_controller import web_threads
 
 registrable_controllers = [
     users,
     subreddits,
     threads,
     web_users,
-    web_reddits
+    web_reddits,
+    web_threads,
 ]
